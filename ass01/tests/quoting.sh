b1a273abb496e187e672e47c9854a8c4236141c6
小太 <nospam@xiaotai.org>
2015-09-26 01:28:51 +1000

    Assignment 1: Add a quoting/escaping test

---
+++ ass01/tests/quoting.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# (Modified from posh's test suite)
+# print "|&;<>()$`\\\"' \tThis a continuation\\\n#of\n the", "echo", "line" # and a multi-line comment \
+# call(["or", "not?"])
+echo \|\&\;\<\>\(\)\$\`\\\"\'\ \	\
+'This a c'on''''''tinuatio'n\
+#of
+ t'he echo li\ne # and a multi-line comment \
+or not\?
