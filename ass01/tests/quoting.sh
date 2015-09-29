bdf1d3579b8d8243723ad34afd72511c06a337cd
小太 <nospam@xiaotai.org>
2015-09-30 01:36:30 +1000

    Assignment 1: Support parsing double quotes

--- ass01/tests/quoting.sh
+++ ass01/tests/quoting.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
 # (Modified from posh's test suite)
-# print "|&;<>()$`\\\"' \tThis a continuation\\\n#of\n the", "echo", "line" # and a multi-line comment \
+
+# print "|&;<>()$`\\\"' \tThis a continuation\\\n#of\n the", "echo line" # and a multi-line comment \
 # subprocess.call(["or", "not?"])
 echo \|\&\;\<\>\(\)\$\`\\\"\'\ \	\
-'This a c'on''''''tinuatio'n\
+'This a c'on''''''tin""""uatio'n\
 #of
- t'he echo li\ne # and a multi-line comment \
+ t'he ec"ho\
+ "li\ne # and a multi-line comment \
 or not\?
+
+FULLVARIABLE="Two words"
+
+# print "One $EMPTYVARIABLE"
+# print "$(echo cmdsubst $FULLVARIABLE)"
+# print "`echo backquot $FULLVARIABLE`"
+# print "One {0}".format(EMPTYVARIABLE)
+# print " ".join("cmdsubst", FULLVARIABLE)
+# print " ".join("backquot", FULLVARIABLE)
+echo 'One $EMPTYVARIABLE'
+echo '$(echo cmdsubst $FULLVARIABLE)'
+echo '`echo backquot $FULLVARIABLE`'
+echo "One $EMPTYVARIABLE"
+echo "$(echo cmdsubst $FULLVARIABLE)"
+echo "`echo backquot $FULLVARIABLE`"
