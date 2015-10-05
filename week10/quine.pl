fc949608a4318a060dec30cc8bbef17f0c9e7b63
小太 <nospam@kota.moe>
2015-10-06 01:02:21 +1100

    Week 10: Finish first two challenge tasks

---
+++ week10/quine.pl
@@ -0,0 +1,1 @@
+$a='$a=%s;printf($a.chr(10),chr(39).$a.chr(39))';printf($a.chr(10),chr(39).$a.chr(39))
---
+++ week10/quinepy.pl
@@ -0,0 +1,1 @@
+$q=chr(39);$a='q=chr(39);a=%s;b=%s;print a%%(q+b+q,q+a+q)';$b='$q=chr(39);$a=%s;$b=%s;printf($a.chr(10),$q.$b.$q,$q.$a.$q)';printf($a.chr(10),$q.$b.$q,$q.$a.$q)
